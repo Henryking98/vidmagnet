f15f4361d8a769d46f3e52aad4c6ca335014f349
Henry <94872110+Henryking98@users.noreply.github.com>
2025-06-24 23:10:45 +0100

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    output: 'export',
 };
 
 export default nextConfig;
